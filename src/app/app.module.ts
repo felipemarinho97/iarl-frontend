427a800142ca99ef059fffef91137d6737bd5d2f
AndersonVidal <anderson.vidal@ccc.ufcg.edu.br>
2018-01-23 11:24:16 -0300

    loginPage & Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
