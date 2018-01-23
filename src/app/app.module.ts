0b58efdeaf8999f528b36144b529dd2d89146f07
AndersonVidal <anderson.vidal@ccc.ufcg.edu.br>
2018-01-23 17:04:35 -0300

    nao comitar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FooterComponent } from './footer/footer.component';
+
+import { LoginModule } from './login/login.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FooterComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastModule.forRoot(),
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { ViewContainerRef } from '@angular/core/src/linker/view_container_ref';
+import { FooterComponent } from '../footer/footer.component';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  entryComponents: [FooterComponent],
 })
 export class LoginComponent implements OnInit {
 
   username = '';
   password = '';
 
-  constructor() { }
+  constructor(public toastr: ToastsManager, vcr: ViewContainerRef) {
+    this.toastr.setRootViewContainerRef(vcr);
+   }
 
   ngOnInit() {
   }
 
-  /*onClickMe(f) {
-    console.log(f);
-    if ( this.username === '') {
-      alert('Usuario vazio!');
-    } else if ( this.password === '' ) {
-      alert('Senha vazia!');
+  onClickMe(f) {
+    if ( this.username === '' || this.password === '' ) {
+      this.toastr.warning('Usuário ou senha inválido!', 'Alerta!');
     }
-  }*/
+  }
 }
