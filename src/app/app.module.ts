f31436c7b86fba7350c8ef636960ef0cc74030ae
AndersonVidal <anderson.vidal@ccc.ufcg.edu.br>
2018-01-23 15:03:45 -0300

    background e css login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -14,7 +14,7 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = '';
+  password = '';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  /*onClickMe(f) {
+    console.log(f);
+    if ( this.username === '') {
+      alert('Usuario vazio!');
+    } else if ( this.password === '' ) {
+      alert('Senha vazia!');
+    }
+  }*/
 }
