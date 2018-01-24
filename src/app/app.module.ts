32513f8a7f4a741871334a48c1a699fbe5d26bb2
AndersonVidal <anderson.vidal@ccc.ufcg.edu.br>
2018-01-24 09:18:58 -0300

    implementação do modulo de rotas e rota /login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { ToastModule, ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
+
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -20,8 +23,9 @@ import { FooterComponent } from './footer/footer.component';
     FormsModule,
     BrowserAnimationsModule,
     ToastModule.forRoot(),
+    AppRoutingModule,
   ],
   providers: [ ToastsManager ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
